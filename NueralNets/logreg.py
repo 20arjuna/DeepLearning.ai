8bc946530311c087377e01bc9e4278366c3c9b5b
Arjun Akkiraju <20arjuna@students.harker.org>
2020-08-02 17:41:17 -0700

    added backprop and gradient descent

--- NueralNets/logreg.py
+++ NueralNets/logreg.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils import *
+import math
 
 def load_data(filename):
     return np.genfromtxt(filename, delimiter=',')
@@ -8,6 +9,29 @@ def predict(X, w, b):
     z = dot_product(X, w) + b
     a = sigmoid(z)
     return a
+
+''' 
+Calculates the derivatives
+'''
+def backprop(X, yhat, y, features):
+    loss = -1 * (y * math.log(yhat) + (1 - y) * (math.log(1 - yhat)))
+    dz = yhat - y
+    db = dz
+    dw = np.empty(features)
+
+    for i in range(features):
+        dw[i] = X[i] * dz
+
+    return dw, db, loss
+
+'''
+Updates the weights
+'''
+def gradient_descent(w, dw, alpha):
+    features = len(w)
+    for i in range(features):
+        w[i] -= alpha * dw[i]
+    return w
 
 if __name__ == '__main__':
     myFile = load_data('pima-indians-diabetes.csv')
