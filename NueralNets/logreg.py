e9c6705ebb06cd16abe95231ac4421b2b63f18ac
Arjun Akkiraju <20arjuna@students.harker.org>
2020-08-03 14:39:01 -0700

    new approach is not working

--- NueralNets/logreg.py
+++ NueralNets/logreg.py
@@ -8,7 +8,11 @@ def load_data(filename):
 def predict(X, w, b):
     z = dot_product(X, w) + b
     yhat = sigmoid(z)
-    return yhat
+
+    if(yhat >= 0.5):
+        return 1.0
+    else:
+        return 0.0
 
 '''
 Calculates the loss for a given prediction 
@@ -20,75 +24,82 @@ def get_loss(yhat, y):
 ''' 
 Calculates the derivatives
 '''
-def backprop(X, yhat, y, features):
-    dz = yhat - y
-    db = dz
-    dw = np.empty(features)
+def backprop(w, b, X, Y):
+    #print(X)
+    A = sigmoid(dot_product(w, X) + b)
+    m = len(X)
 
-    for i in range(features):
+    loss = -(Y*np.log(A) + (1-Y)*np.log(1-A))
+    cost = (1/m) * np.sum(loss)
+
+    dz = A - Y
+    db = dz
+    dw = np.zeros(len(w))
+
+    for i in range(len(dw)):
         dw[i] = X[i] * dz
 
-    return [dw, db]
+    gradients = {"dw": dw,
+                 "db": db}
+
+    return gradients, cost
+
 
 '''
-Updates the weights
+Updates the weights and biases
+
+@return w the updates weights array
+@return b the updated bias
 '''
-def gradient_descent(w, dw, alpha):
+def gradient_descent(w, b, X, Y, iterations, alpha):
     features = len(w)
-    for i in range(features):
-        w[i] -= alpha * dw[i]
-    return w
+    print(X[0])
+    print(type(X[0]))
+    print(X[0].shape)
+    for i in range(iterations):
+        gradients, cost = backprop(w, b, X[i], Y[i])
+
+        dw = gradients["dw"]
+        db = gradients["db"]
+
+        b -= alpha * db
+        for i in range(features):
+            w[i] -= alpha * dw[i]
+
+        if(i%100 == 0):
+            print("Cost after iteration #" + str(i) + ": " + str(cost))
+
+    return w, b
 
 if __name__ == '__main__':
     myFile = load_data('pima-indians-diabetes.csv')
 
-    X_train = myFile[:384, :-1]
+    X_train = myFile[:384,:-1]
+    #print(X_train[0])
     Y_train = myFile[:384, -1]
+    #print(Y_train[0])
 
-    X_test = myFile[384:, :-1]
+    X_test = myFile[384:,:-1]
+    #print(X_test[0])
     Y_test = myFile[384:, -1]
+    #print(Y_test[0])
 
     features = len(X_train[0])
-    m = len(X_train)
+
 
     test_no = 5
     print(X_test[test_no])
     print(Y_test[test_no])
 
     w = np.zeros(features)
-    b = np.random.random()
+    b = 0
 
     print("\n" + "weights: " + str(w))
     print("bias: " + str(b))
     print("sample prediction: " + str(predict(X_test[test_no], w, b)))
+    print("__________________________________________________________")
 
-    dw = np.zeros(features)
-    db = 0
-    alpha = 0.05
-    for iter in range(100):
-        for i in range(m):
-            yhat = predict(X_train[i], w, b)
-            if(yhat != 1.0 and yhat != 0.0):
-                print("yhat: " + str(yhat))
-                print("Y_train[i]" + str(Y_train[i]))
-                print()
-                loss = get_loss(yhat, Y_train[i])
-                print("\n" + "loss: " + str(loss))
-                dz = yhat - loss
-
-                for j in range(features):
-                    dw[j] += X_train[i][j] * dz
-                db += dz
-
-            loss /= m
-            dw /= m
-            db /= m
-
-            for i in range(features):
-                w[i] -= (alpha * dw[i])
-
-            b -= (alpha * db)
-            # print("REACHED HERE" + "\n")
+    w, b = gradient_descent(w, b, X_train, Y_train, 1000, 0.01)
 
     print()
     print("weights: " + str(w))
