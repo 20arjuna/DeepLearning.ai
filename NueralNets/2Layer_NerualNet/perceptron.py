942310a2bf3eabdec8b48c0928f78fea32f45c89
Arjun Akkiraju <20arjuna@students.harker.org>
2020-08-09 17:42:08 -0700

    perceptron predict works

--- NueralNets/2Layer_NerualNet/perceptron.py
+++ NueralNets/2Layer_NerualNet/perceptron.py
@@ -15,13 +15,25 @@ class Perceptron:
         self.features = features.reshape(len(features),1)
         self.hiddenLayerNodes = hiddenLayerNodes
         self.outputNodes = 1
-        self.bias = np.random.randn(hiddenLayerNodes, 1)
-        self.w1 = np.random.randn(hiddenLayerNodes, features)
+        self.b1 = np.random.randn(self.hiddenLayerNodes, 1)
+        self.b2 = np.random.randn(self.outputNodes, 1)
+        self.w1 = np.random.randn(self.hiddenLayerNodes, len(features))
+        self.w2 = np.random.randn(self.outputNodes, self.hiddenLayerNodes)
 
     def predict(self):
-        z1 = np.dot(self.weights[0], self.features) + self.bias
-        a1 = sigmoid(z1)
-        z2 =
+        z1 = np.dot(self.w1, self.features) + self.b1
+        a1 = np.zeros(z1.shape)
+        for i in range(len(z1)):
+            for j in range(len(z1[0])):
+                a1[i][0] = sigmoid(z1[i][0])
+        print(a1)
+        z2 = np.dot(self.w2, a1) + self.b2
+        a2 = sigmoid(z2)
+        return a2
 
 
 if __name__ == '__main__':
+    inputs = np.array([0, 1, 1])
+    hidden = 4
+    myNeuralNet = Perceptron(inputs, hidden)
+    print(myNeuralNet.predict())
